8e4a60e2fbecbded59ed05f227bcfd9de2b14480
mwandi <frankmwandi93@gmail.com>
2024-03-09 15:52:44 +0300

    adding first for asynch tasks

---
+++ simpleapp/consumer.py
@@ -0,0 +1,18 @@
+from channels.generic.websocket import WebsocketConsumer
+
+class EchoConsumer(WebsocketConsumer):
+    
+    def connect(self):
+        """Event when client connects"""
+        # Informs client of successful connection
+        self.accept()
+        # Send message to client
+        self.send(text_data="You are connected byWebSockets!")
+ 
+    def disconnect(self, close_code):
+        """Event when client disconnects"""
+        pass
+    
+    def receive(self, text_data):
+        """Event when data is received"""
+        pass
